bdbd4139430ea0d9d08758b836e51632576637ce
twentiecker <twentiecker@gmail.com>
2026-02-19 11:43:52 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.mount("#app");
